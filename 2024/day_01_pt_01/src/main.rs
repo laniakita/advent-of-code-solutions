19dd5f569fcd217965c5a9e99bd0b902a5df4a71
laniakita <101172492+laniakita@users.noreply.github.com>
2024-12-02 15:02:49 -1000

    cleanup day 01 code

--- 2024/day_01_pt_01/src/main.rs
+++ 2024/day_01_pt_01/src/main.rs
@@ -2,9 +2,7 @@ use std::{env, process};
 use day_01_pt_01::Config;
 
 fn main() {
-    //println!("{ids_01:?}");
-    
-    //let hard_input = Path::new("./day_01_pt_01/input.txt");
+    // ex: file_path = Path::new("./day_01_pt_01/input.txt");
 
     let args: Vec<String> = env::args().collect();
 
@@ -20,7 +18,3 @@ fn main() {
         process::exit(1);
     }
 }
-
-
-
-
